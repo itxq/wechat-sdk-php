ea3fcd1d80631742b48ab3fb738d49b7d300cbc5
IT小强xqitw.cn <360237521@qq.com>
2018-09-19 16:14:46 +0800

    工具类优化

--- src/lib/Tools.php
+++ src/lib/Tools.php
@@ -65,4 +65,111 @@ class Tools
         }
         return $value;
     }
+    
+    /**
+     * 判断是否为微信浏览器
+     * @return bool
+     */
+    public static function isMicroMessenger() {
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * 数组序列化url字符串
+     * @param $array - 数组（键值对）
+     * @return string
+     */
+    public static function arraySerialize($array) {
+        $str = '';
+        if (!is_array($array) || count($array) < 1) {
+            return $str;
+        }
+        foreach ($array as $k => $v) {
+            $str .= (empty($str)) ? '?' : '&';
+            $str .= $k . '=' . $v;
+        }
+        return $str;
+    }
+    
+    /**
+     * 检测是否使用手机访问
+     * @access public
+     * @return bool
+     */
+    public static function isMobile() {
+        if (self::server('HTTP_VIA') && stristr(self::server('HTTP_VIA'), "wap")) {
+            return true;
+        } elseif (self::server('HTTP_ACCEPT') && strpos(strtoupper(self::server('HTTP_ACCEPT')), "VND.WAP.WML")) {
+            return true;
+        } elseif (self::server('HTTP_X_WAP_PROFILE') || self::server('HTTP_PROFILE')) {
+            return true;
+        } elseif (self::server('HTTP_USER_AGENT') && preg_match('/(blackberry|configuration\/cldc|hp |hp-|htc |htc_|htc-|iemobile|kindle|midp|mmp|motorola|mobile|nokia|opera mini|opera |Googlebot-Mobile|YahooSeeker\/M1A1-R2D2|android|iphone|ipod|mobi|palm|palmos|pocket|portalmmm|ppc;|smartphone|sonyericsson|sqh|spv|symbian|treo|up.browser|up.link|vodafone|windows ce|xda |xda_)/i', self::server('HTTP_USER_AGENT'))) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    /**
+     * 获取server参数
+     * @param  string $name 数据名称
+     * @param  string|null $default 默认值
+     * @return null|string|array
+     */
+    public static function server($name = '', $default = null) {
+        if (empty($name)) {
+            return $_SERVER;
+        } else {
+            $name = strtoupper($name);
+        }
+        return isset($_SERVER[$name]) ? $_SERVER[$name] : $default;
+    }
+    
+    /**
+     * 生成随机字符串
+     * @param int $length - 指定生成字符串的长度
+     * @param string $type - 指定生成字符串的类型（all-全部，num-纯数字，letter-纯字母）
+     * @return null|string
+     */
+    public static function cmRound($length = 4, $type = 'all') {
+        $str = '';
+        $strUp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $strLow = 'abcdefghijklmnopqrstuvwxyz';
+        $number = '0123456789';
+        switch ($type) {
+            case 'num':
+                $strPol = $number;
+                break;
+            case 'letter':
+                $strPol = $strUp . $strLow;
+                break;
+            default:
+                $strPol = $strUp . $number . $strLow;
+        }
+        $max = strlen($strPol) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $str .= $strPol[mt_rand(0, $max)];
+        }
+        return $str;
+    }
+    
+    /**
+     * 获取当前请求的URL
+     * @return string
+     */
+    public static function getUrl() {
+        $ssl = isset($_SERVER['HTTPS']) ? strip_tags($_SERVER['HTTPS']) : false;
+        $server_name = strip_tags($_SERVER['SERVER_NAME']);
+        $request_url = strip_tags($_SERVER['REQUEST_URI']);
+        if (!$ssl || $ssl != 'on') {
+            $url = 'http://' . $server_name . $request_url;
+        } else {
+            $url = 'https://' . $server_name . $request_url;
+        }
+        return $url;
+    }
 }
